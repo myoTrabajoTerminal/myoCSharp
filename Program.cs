887124d94fef88d1dcfe8b509f0c5b7fd52e6443
Alex <medinas.alex0001@gmail.com>
2019-04-03 18:41:34 -0600

    Primer commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace myoCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
